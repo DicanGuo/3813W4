1421a1a2a5bea9c398e42a1d7e4b1415ec34c2af
DicanGuo <87957743+DicanGuo@users.noreply.github.com>
2022-08-26 10:54:29 +1000

    issue fixed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,22 +7,31 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-    userid: any = '';
+    userid: string = '';
     password: string = '';
-    paramsub: any;
+    errorMassage = "User credential not match";
+    
+    users = [
+      {'userid': '1', 'password': '123'},
+      {'userid': '2', 'password': '123'},
+      {'userid': '3', 'password': '123'}
+    ]
 
   constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.router.navigateByUrl('/login');
-    this.paramsub = this.route.paramMap.subscribe(params => {
-      this.userid = params.get('id');
-    });
   }
 
   itemClicked(){
-    console.log(this.userid, this.password);
+    // console.log(this.userid, this.password);
     // this.router.navigate('/account:userid')
-
+    for (let i in this.users){
+      if (this.userid == this.users[i].userid && this.password == this.users[i].password) {
+        this.router.navigateByUrl('/account/' + this.userid);
+      }
+      else {
+        alert(this.errorMassage);
+      }
+    }
   }
-} 
+}
